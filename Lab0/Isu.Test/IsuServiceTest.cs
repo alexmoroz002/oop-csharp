e58f01d465fa357969bc43325b3a3c7149fd995a
Alex Moroz <morozo.irina2009@gmail.com>
2022-09-17 18:54:47 +0300

    feat: implement CreateGroup and ReachMaxStudent tests

--- Lab0/Isu.Test/IsuServiceTest.cs
+++ Lab0/Isu.Test/IsuServiceTest.cs
@@ -1,3 +1,5 @@
+using Isu.Entities;
+using Isu.Models;
 using Isu.Services;
 using Xunit;
 
@@ -6,18 +8,41 @@ namespace Isu.Test;
 public class IsuServiceTest
 {
     [Fact]
-    public void AddStudentToGroup_StudentHasGroupAndGroupContainsStudent() { }
+    public void AddStudentToGroup_StudentHasGroupAndGroupContainsStudent()
+    {
+        var service = new IsuService();
+    }
 
-    [Fact]
-    public void ReachMaxStudentPerGroup_ThrowException() { }
+    [Theory]
+    [InlineData(10, 5)]
+    [InlineData(5, 6)]
+    [InlineData(6, 6)]
+    public void ReachMaxStudentPerGroup_ThrowException(int groupSize, int studentsCount)
+    {
+        var service = new IsuService();
+        Group group = service.AddGroup(new GroupName("M32071"), groupSize);
+        for (int i = 0; i < studentsCount; i++)
+        {
+            service.AddStudent(group, "Ivan Ivanov");
+        }
+    }
 
-    [Fact]
-    public void CreateGroupWithInvalidName_ThrowException() { }
+    [Theory]
+    [InlineData("M32071")]
+    [InlineData("M32071c")]
+    [InlineData("q329c")]
+    [InlineData("l9999")]
+    [InlineData("a5599")]
+    public void CreateGroupWithInvalidName_ThrowException(string groupName)
+    {
+        var service = new IsuService();
+        service.AddGroup(new GroupName(groupName));
+    }
 
     [Fact]
     public void TransferStudentToAnotherGroup_GroupChanged()
     {
-        IIsuService service = new IsuService();
+        IsuService service = new IsuService();
     }
 
     /*
