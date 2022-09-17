3b14bfed5a5ffce9b7f6e1db8c6e7782fbb8915e
alexmoroz002 <morozo.alexey2003@gmail.com>
2022-09-17 14:45:56 +0300

    refactor: change group pattern behavior

--- Lab0/Isu/Models/GroupName.cs
+++ Lab0/Isu/Models/GroupName.cs
@@ -11,15 +11,6 @@ public class GroupName
         Name = name;
     }
 
-    private enum EducationLevel
-    {
-        Bachelor = 3,
-        Magistracy = 4,
-        Specialty = 5,
-        Graduate = 7,
-        Doctoral = 8,
-    }
-
     public string Name
     {
         get => _name;
@@ -27,7 +18,7 @@ public class GroupName
         private set
         {
             var patternRegex = new Regex(
-                    @"^(([A-Z][3][1-4]|[A-Z][4][1-2]|[B][5][1-6]|[7-8][0-9])([0-9][0-9])([1-4]?[c]?))$",
+                    @"^([A-Z]([3][1-4]|[4][1-2]|[5][1-6])([0-9][0-9])([1-4]?[c]?))$",
                     RegexOptions.IgnoreCase);
             if (!patternRegex.IsMatch(value))
                 return;
