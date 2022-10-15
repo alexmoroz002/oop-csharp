3d9309bca6e8d2748a2edcd563e670019d895aef
alexmoroz002 <morozo.alexey2003@gmail.com>
2022-10-15 13:24:58 +0300

    feat: implement class for products to buy

--- Lab1/Shops/Models/ProductToBuy.cs
+++ Lab1/Shops/Models/ProductToBuy.cs
@@ -2,5 +2,21 @@
 
 public class ProductToBuy
 {
-    
+    private readonly string _name;
+    private int _count;
+
+    public ProductToBuy(string name, int count = 0)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentNullException("name");
+        if (count < 0)
+            throw new ArgumentOutOfRangeException("count");
+
+        _name = name;
+        _count = count;
+    }
+
+    public string Name => _name;
+
+    public int Count => _count;
 }
