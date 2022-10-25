bd43c43d238426fefa6a288f9054c5de3d689fe9
alexmoroz002 <morozo.alexey2003@gmail.com>
2022-10-25 06:13:08 +0300

    feat: implement Teacher class

---
+++ Lab2/Isu.Extra/Entities/Teacher.cs
@@ -0,0 +1,13 @@
+﻿namespace Isu.Extra.Entities;
+
+public class Teacher
+{
+    public Teacher(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public int Id { get; }
+    public string Name { get; }
+}
