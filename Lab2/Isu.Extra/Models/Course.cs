1007bb9bea631a8fcc02de3f0055e2e8dc6aca8a
alexmoroz002 <morozo.alexey2003@gmail.com>
2022-10-25 06:12:52 +0300

    feat: add basic classes

---
+++ Lab2/Isu.Extra/Models/Course.cs
@@ -0,0 +1,8 @@
+﻿using Isu.Extra.Entities;
+
+namespace Isu.Extra.Models;
+
+public abstract class Course
+{
+    internal List<Flow> Flows { get; set; }
+}
---
+++ Lab2/Isu.Extra/Models/Timetable.cs
@@ -0,0 +1,8 @@
+﻿using Isu.Extra.Entities;
+
+namespace Isu.Extra.Models;
+
+public class Timetable
+{
+    private List<Lesson> _lessons;
+}
