1385f36560d1fd31a795366359ac8dd6006e37a6
alexmoroz002 <morozo.alexey2003@gmail.com>
2022-10-29 04:47:18 +0300

    feat: complete IsuServiceExtra and interface

--- Lab2/Isu.Extra/Services/IIsuServiceExtra.cs
+++ Lab2/Isu.Extra/Services/IIsuServiceExtra.cs
@@ -1,6 +1,18 @@
-﻿namespace Isu.Extra.Services;
+﻿using Isu.Entities;
+using Isu.Extra.Entities;
+
+namespace Isu.Extra.Services;
 
 public interface IIsuServiceExtra
 {
-    
+    public OgnpCourse AddCourse(char faculty, string name);
+    public CourseFlow AddFlow(OgnpCourse course);
+    public CourseGroup AddGroup(CourseFlow flow, params Lesson[] lessons);
+    public void AddRecordOnCourse(Student student, OgnpCourse course, CourseFlow flow, CourseGroup group);
+    public void DeleteRecordOnCourse(Student student, OgnpCourse course, CourseFlow flow, CourseGroup group);
+    public IReadOnlyList<CourseFlow> FindFlows(OgnpCourse course);
+    public IReadOnlyList<Student> FindStudents(OgnpCourse course);
+    public OgnpCourse FindCourse(string courseName);
+    public IReadOnlyList<Student> FindUnsignedOgnpStudents(Group group);
+    public IReadOnlyList<CourseGroup> FindGroups(OgnpCourse course, CourseFlow flow);
 }
