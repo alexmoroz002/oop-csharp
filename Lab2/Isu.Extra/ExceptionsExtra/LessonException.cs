4a27bece92293b57b8c89256b47d7a98edb6a5e4
alexmoroz002 <morozo.alexey2003@gmail.com>
2022-10-29 04:43:48 +0300

    feat: implement exceptions

---
+++ Lab2/Isu.Extra/ExceptionsExtra/LessonException.cs
@@ -0,0 +1,17 @@
+﻿namespace Isu.Extra.ExceptionsExtra;
+
+public class LessonException : Exception
+{
+    private LessonException(string message)
+        : base(message) { }
+
+    public static LessonException InvalidLessonNumber(int number)
+    {
+        return new LessonException($"{number} is invalid");
+    }
+
+    public static LessonException NullLesson()
+    {
+        return new LessonException("Lesson is null");
+    }
+}
---
+++ Lab2/Isu.Extra/ExceptionsExtra/OgnpCourseException.cs
@@ -0,0 +1,20 @@
+﻿using Isu.Entities;
+
+namespace Isu.Extra.ExceptionsExtra;
+
+public class OgnpCourseException : Exception
+{
+    private OgnpCourseException(string message)
+        : base(message) { }
+
+    public static OgnpCourseException InvalidFacultyLetter(char faculty)
+    {
+        return new OgnpCourseException($"Letter {faculty} is invalid faculty letter");
+    }
+
+    public static OgnpCourseException StudentAndOgnpFacultyIsSame(Student student, char faculty)
+    {
+        return new OgnpCourseException(
+            $"Student {student.Name} can't sign up to Ognp course from his faculty {faculty}");
+    }
+}
