607e80f73a239ae6ba59e3422c64d784b11e9ee7
alexmoroz002 <morozo.alexey2003@gmail.com>
2022-10-29 04:47:44 +0300

    feat: implement Timetable

--- Lab2/Isu.Extra/Models/Timetable.cs
+++ Lab2/Isu.Extra/Models/Timetable.cs
@@ -1,8 +1,45 @@
 ﻿using Isu.Extra.Entities;
+using Isu.Extra.ExceptionsExtra;
 
 namespace Isu.Extra.Models;
 
 public class Timetable
 {
-    private List<Lesson> _lessons;
+    private List<Lesson> _lessons = new ();
+
+    public Timetable()
+    {
+    }
+
+    public Timetable(params Lesson[] lessons)
+    {
+        if (lessons.Length < 1)
+            throw TimetableException.EmptyLessonList();
+        if (lessons.Distinct().Count() < lessons.Length)
+            throw TimetableException.TimetableIntersection();
+        foreach (Lesson lesson in lessons)
+        {
+            if (lesson == null)
+                throw LessonException.NullLesson();
+            _lessons.Add(lesson);
+        }
+    }
+
+    public IReadOnlyList<Lesson> Lessons => _lessons;
+
+    public void AddLessons(params Lesson[] lessons)
+    {
+        foreach (Lesson lesson in lessons)
+        {
+            _lessons.Add(lesson);
+        }
+    }
+
+    public void DeleteLessons(params Lesson[] lessons)
+    {
+        foreach (Lesson lesson in lessons)
+        {
+            _lessons.Remove(lesson);
+        }
+    }
 }
