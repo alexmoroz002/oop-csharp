5ea5f516cc8ec0bfbaa2455be962f13e9559ae45
alexmoroz002 <morozo.alexey2003@gmail.com>
2022-10-29 04:44:17 +0300

    feat: complete decorator

--- Lab2/Isu.Extra/Decorators/IsuBaseDecorator.cs
+++ Lab2/Isu.Extra/Decorators/IsuBaseDecorator.cs
@@ -4,55 +4,55 @@ using Isu.Services;
 
 namespace Isu.Extra.Decorators;
 
-public class IsuDecorator : IIsuService
+public class IsuBaseDecorator : IIsuService
 {
-    public IsuDecorator(IIsuService wrapper)
+    protected IsuBaseDecorator(IIsuService wrapper)
     {
         Wrapper = wrapper;
     }
 
     protected IIsuService Wrapper { get; set; }
-    public Group AddGroup(GroupName groupName, int studentsCount)
+    public virtual Group AddGroup(GroupName groupName, int studentsCount)
     {
         return Wrapper.AddGroup(groupName, studentsCount);
     }
 
-    public Student AddStudent(Group group, string name)
+    public virtual Student AddStudent(Group group, string name)
     {
         return Wrapper.AddStudent(group, name);
     }
 
-    public Student GetStudent(int id)
+    public virtual Student GetStudent(int id)
     {
         return Wrapper.GetStudent(id);
     }
 
-    public Student FindStudent(int id)
+    public virtual Student FindStudent(int id)
     {
         return Wrapper.FindStudent(id);
     }
 
-    public IReadOnlyList<Student> FindStudents(GroupName groupName)
+    public virtual IReadOnlyList<Student> FindStudents(GroupName groupName)
     {
         return Wrapper.FindStudents(groupName);
     }
 
-    public IReadOnlyList<Student> FindStudents(CourseNumber courseNumber)
+    public virtual IReadOnlyList<Student> FindStudents(CourseNumber courseNumber)
     {
         return Wrapper.FindStudents(courseNumber);
     }
 
-    public Group FindGroup(GroupName groupName)
+    public virtual Group FindGroup(GroupName groupName)
     {
         return Wrapper.FindGroup(groupName);
     }
 
-    public IReadOnlyList<Group> FindGroups(CourseNumber courseNumber)
+    public virtual IReadOnlyList<Group> FindGroups(CourseNumber courseNumber)
     {
         return Wrapper.FindGroups(courseNumber);
     }
 
-    public void ChangeStudentGroup(Student student, Group newGroup)
+    public virtual void ChangeStudentGroup(Student student, Group newGroup)
     {
         Wrapper.ChangeStudentGroup(student, newGroup);
     }
