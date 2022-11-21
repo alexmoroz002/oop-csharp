f9f26cc367059611cfbffeb0072be85701df1701
Alex Moroz <morozo.alexey2003@gmail.com>
2022-11-21 08:37:43 +0300

    feat: implement storage, backup classes

--- Lab3/Backups/Entities/Backup.cs
+++ Lab3/Backups/Entities/Backup.cs
@@ -2,5 +2,10 @@
 
 public class Backup
 {
-    
+    private List<RestorePoint> _restorePoints;
+
+    public RestorePoint CreateRestorePoint()
+    {
+
+    }
 }
--- Lab3/Backups/Entities/Storage.cs
+++ Lab3/Backups/Entities/Storage.cs
@@ -1,6 +1,10 @@
-﻿namespace Backups.Entities;
+﻿using System.IO.Compression;
+using Zio;
+
+namespace Backups.Entities;
 
 public class Storage
 {
-    
+    private UPath _path;
+    private List<ZipArchive> _archives;
 }
