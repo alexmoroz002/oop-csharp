842d54bbd6b799e6a6675aeb1dabe1aadfcc73f1
Alex Moroz <morozo.alexey2003@gmail.com>
2022-11-21 08:36:22 +0300

    feat: implement backup task

--- Lab3/Backups/Entities/BackupTask.cs
+++ Lab3/Backups/Entities/BackupTask.cs
@@ -1,8 +1,30 @@
-﻿using Backups.Models;
+﻿using Backups.Interfaces;
+using Backups.Models;
 
 namespace Backups.Entities;
 
-public class BackupTask
+public class BackupTask : IBackupTask
 {
-    private IEnumerable<BackupObject> trackedObjects;
+    private Config _config;
+    private Backup _backup;
+
+    public BackupTask(Config config)
+    {
+        _config = config;
+    }
+
+    public void CreateBackup()
+    {
+        _config.
+    }
+
+    public void CheckObjectsToBackup(params BackupObject[] objects)
+    {
+        _config.AddObjects(objects);
+    }
+
+    public void UncheckObjectsToBackup(params BackupObject[] objects)
+    {
+        _config.DeleteObjects(objects);
+    }
 }
---
+++ Lab3/Backups/Interfaces/IBackupTask.cs
@@ -0,0 +1,10 @@
+﻿using Backups.Models;
+
+namespace Backups.Interfaces;
+
+public interface IBackupTask
+{
+    void CreateBackup();
+    void CheckObjectsToBackup(params BackupObject[] objects);
+    void UncheckObjectsToBackup(params BackupObject[] objects);
+}
