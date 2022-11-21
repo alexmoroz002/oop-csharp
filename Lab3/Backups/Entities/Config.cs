6dd956e1cea31a88367f7d033a00c2be48a3c298
Alex Moroz <morozo.alexey2003@gmail.com>
2022-11-21 08:36:45 +0300

    feat: implement config

--- Lab3/Backups/Entities/Config.cs
+++ Lab3/Backups/Entities/Config.cs
@@ -5,6 +5,36 @@ namespace Backups.Entities;
 
 public class Config
 {
-    private IAlgorithm algorithm;
-    private Repository repository;
+    private IAlgorithm _algorithm;
+    private Repository _repository;
+    private List<BackupObject> _trackedObjects;
+
+    public Config(IAlgorithm algorithm, Repository repository)
+    {
+        _algorithm = algorithm;
+        _repository = repository;
+        _trackedObjects = new List<BackupObject>();
+    }
+
+    public void AddObjects(params BackupObject[] objects)
+    {
+        if (objects == null)
+        {
+            throw new NotImplementedException();
+        }
+
+        if (objects.Any(x => x == null))
+            throw new NotImplementedException();
+        _trackedObjects.AddRange(objects);
+    }
+
+    public void DeleteObjects(params BackupObject[] objects)
+    {
+        if (objects == null)
+        {
+            throw new NotImplementedException();
+        }
+
+        _trackedObjects = _trackedObjects.Except(objects).ToList();
+    }
 }
