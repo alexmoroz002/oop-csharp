938f8d64381e016a22af8d2328470f9b1c8fae32
Alex Moroz <morozo.alexey2003@gmail.com>
2022-11-21 08:38:03 +0300

    feat: implement restore point

--- Lab3/Backups/Entities/RestorePoint.cs
+++ Lab3/Backups/Entities/RestorePoint.cs
@@ -4,6 +4,17 @@ namespace Backups.Entities;
 
 public class RestorePoint
 {
-    private IEnumerable<BackupObject> objects;
-    private DateTime creationTime;
+    private List<BackupObject> _objects;
+    private DateTime _creationTime;
+
+    public RestorePoint(List<BackupObject> objects)
+    {
+        _objects = objects;
+        _creationTime = DateTime.Now;
+    }
+
+    public void SetTIme(DateTime time)
+    {
+        _creationTime = time;
+    }
 }
