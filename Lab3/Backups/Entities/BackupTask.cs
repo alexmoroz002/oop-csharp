1d36fd82bc3b57065ab5bdabeca5d0623fb1f2b0
Alex Moroz <morozo.alexey2003@gmail.com>
2022-11-22 14:00:30 +0300

    feat: add constructor

--- Lab3/Backups/Entities/BackupTask.cs
+++ Lab3/Backups/Entities/BackupTask.cs
@@ -7,23 +7,33 @@ public class BackupTask : IBackupTask
 {
     private Config _config;
     private Backup _backup;
+    private string _name;
 
-    public BackupTask(Config config)
+    public BackupTask(string name, Config config)
     {
+        _backup = new Backup();
         _config = config;
+        _name = name;
     }
 
-    public void CreateBackup()
+    public BackupTask(string name, Repository repository, IAlgorithm algorithm)
     {
-        _config.
+        _backup = new Backup();
+        _config = new Config(algorithm, repository);
+        _name = name;
     }
 
-    public void CheckObjectsToBackup(params BackupObject[] objects)
+    public RestorePoint CreateBackup()
+    {
+        return _backup.CreateRestorePoint();
+    }
+
+    public void CheckObjectsToBackup(params IBackupObject[] objects)
     {
         _config.AddObjects(objects);
     }
 
-    public void UncheckObjectsToBackup(params BackupObject[] objects)
+    public void UncheckObjectsToBackup(params IBackupObject[] objects)
     {
         _config.DeleteObjects(objects);
     }
