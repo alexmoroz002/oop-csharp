6b6a68c668ba6d8df6f1d6b18bf4de235879daba
Alex Moroz <morozo.alexey2003@gmail.com>
2022-11-24 02:09:04 +0300

    feat: add custom exceptions

---
+++ Lab3/Backups/Exceptions/ConfigException.cs
@@ -0,0 +1,24 @@
+﻿using Zio;
+
+namespace Backups.Exceptions;
+
+public class ConfigException : Exception
+{
+    private ConfigException(string e)
+        : base(e) { }
+
+    public static ConfigException NullException()
+    {
+        return new ConfigException("Object is null");
+    }
+
+    public static ConfigException ObjectNameException()
+    {
+        return new ConfigException("Unable to archive multiple objects with same name");
+    }
+
+    public static ConfigException PathIsRelativeException(UPath path)
+    {
+        return new ConfigException($"{path} is not absolute");
+    }
+}
