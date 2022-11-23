6a0da833afabde278b91ff1cb6d1cf4629cc04af
Alex Moroz <morozo.alexey2003@gmail.com>
2022-11-23 15:24:14 +0300

    feat: add visitor draft

---
+++ Lab3/Backups/Visitors/BackupObjectVisitor.cs
@@ -0,0 +1,16 @@
+﻿using Backups.Implementations;
+
+namespace Backups.Visitors;
+
+public class BackupObjectTypeVisitor : IBackupObjectTypeVisitor
+{
+    public void VisitFolder(FolderObject folder)
+    {
+
+    }
+
+    public void VisitFile(FileObject file)
+    {
+
+    }
+}
---
+++ Lab3/Backups/Visitors/IBackupObjectTypeVisitor.cs
@@ -0,0 +1,10 @@
+﻿using Backups.Implementations;
+using Backups.Interfaces;
+
+namespace Backups.Visitors;
+
+public interface IBackupObjectTypeVisitor
+{
+    void VisitFolder(FolderObject folder);
+    void VisitFile(FileObject file);
+}
