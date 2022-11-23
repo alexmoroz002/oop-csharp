88c25302e67051fd04cf361705d69336488bd4b2
Alex Moroz <morozo.alexey2003@gmail.com>
2022-11-23 18:24:02 +0300

    test: add test class with test

---
+++ Lab3/Backups.Test/BackupsTest.cs
@@ -0,0 +1,17 @@
+﻿using Backups.Implementations;
+using Xunit;
+using Zio;
+
+namespace Backups.Test;
+
+public class BackupsTest
+{
+    [Fact]
+    public void CreateDirectoryWithFileInMemoryRepository_RepositoryHasFile()
+    {
+        var repo = new InMemoryRepository();
+        repo.CreateDirectory(UPath.Root / "Test");
+        repo.CreateFile(UPath.Root / "Test" / "1.zip");
+        Assert.True(repo.FileSystem.FileExists(@"/Test/1.zip"));
+    }
+}
