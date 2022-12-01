5c50cb32a3b72c70fce603517d6cda3147f3afa4
Alex Moroz <morozo.alexey2003gmail.com>
2022-12-01 09:09:25 +0300

    feat: create CentralBank Class

---
+++ Lab4/Banks/Services/CentralBank.cs
@@ -0,0 +1,22 @@
+﻿namespace Banks.Services;
+
+public class CentralBank
+{
+    private static CentralBank _bank;
+    private int x;
+
+    private CentralBank(int x)
+    {
+        this.x = x;
+    }
+
+    public static CentralBank GetInstance(int x)
+    {
+        if (_bank == null)
+        {
+            _bank = new CentralBank(x);
+        }
+
+        return _bank;
+    }
+}
