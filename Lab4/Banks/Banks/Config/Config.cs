d667d66087117f75afe7a79f992a4d1fa90196f3
Alex Moroz <morozo.alexey2003@gmail.com>
2022-12-10 02:16:03 +0300

    feat: implement Config class

---
+++ Lab4/Banks/Banks/Config/Config.cs
@@ -0,0 +1,23 @@
+﻿namespace Banks.Banks.Config;
+
+public class Config : IConfig
+{
+    public Config(int debitInterest, int depositInterestFirst, int depositInterestSecond, int depositInterestThird, decimal creditCommission, decimal suspiciousLimit, decimal creditLimit)
+    {
+        DebitInterest = debitInterest;
+        DepositInterestFirst = depositInterestFirst;
+        DepositInterestSecond = depositInterestSecond;
+        DepositInterestThird = depositInterestThird;
+        CreditCommission = creditCommission;
+        SuspiciousLimit = suspiciousLimit;
+        CreditAccountLimit = creditLimit;
+    }
+
+    public int DebitInterest { get; }
+    public int DepositInterestFirst { get; }
+    public int DepositInterestSecond { get; }
+    public int DepositInterestThird { get; }
+    public decimal CreditCommission { get; }
+    public decimal SuspiciousLimit { get; }
+    public decimal CreditAccountLimit { get; }
+}
---
+++ Lab4/Banks/Banks/Config/IConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace Banks.Banks.Config;
+
+public interface IConfig
+{
+    int DebitInterest { get; }
+    int DepositInterestFirst { get; }
+    int DepositInterestSecond { get; }
+    int DepositInterestThird { get; }
+    decimal CreditCommission { get; }
+    decimal SuspiciousLimit { get; }
+    decimal CreditAccountLimit { get; }
+}
