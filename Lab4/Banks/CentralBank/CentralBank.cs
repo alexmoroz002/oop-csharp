c7588d7c8e886a9a30baf08e4d3da08c5806bcb8
Alex Moroz <morozo.alexey2003@gmail.com>
2022-12-10 05:07:33 +0300

    feat: implement time leap machine

--- Lab4/Banks/CentralBank/CentralBank.cs
+++ Lab4/Banks/CentralBank/CentralBank.cs
@@ -1,16 +1,22 @@
-﻿using Banks.Banks;
+﻿using System.Security.Cryptography.X509Certificates;
+using Banks.Banks;
 using Banks.Banks.Config;
 
 namespace Banks.CentralBank;
 
 public class CentralBank : ICentralBank
 {
+    private const int MonthCount = 12;
     private List<IBank> _banks;
+    private int _daysPassed;
+    private int _daysInMonth;
 
     public CentralBank()
     {
         SystemDate = DateOnly.FromDateTime(DateTime.Now);
         _banks = new List<IBank>();
+        _daysPassed = 0;
+        _daysInMonth = DateTime.DaysInMonth(SystemDate.Year, SystemDate.Month);
     }
 
     public DateOnly SystemDate { get; }
@@ -25,16 +31,40 @@ public class CentralBank : ICentralBank
 
     public ICentralBank PlusDay()
     {
-        throw new NotImplementedException();
+        _daysPassed++;
+        if (_daysPassed == _daysInMonth)
+        {
+            _daysInMonth = DateTime.DaysInMonth(SystemDate.Year, SystemDate.Month);
+            _daysPassed = 0;
+            _banks.ForEach(x =>
+            {
+                x.AccumulateDailyPercents();
+                x.AccruePercents();
+            });
+            return this;
+        }
+
+        _banks.ForEach(x => x.AccumulateDailyPercents());
+        return this;
     }
 
     public ICentralBank PlusMonth()
     {
-        throw new NotImplementedException();
+        for (int i = 0; i < _daysInMonth; i++)
+        {
+            PlusDay();
+        }
+
+        return this;
     }
 
     public ICentralBank PlusYear()
     {
-        throw new NotImplementedException();
+        for (int i = 0; i < MonthCount; i++)
+        {
+            PlusMonth();
+        }
+
+        return this;
     }
 }
