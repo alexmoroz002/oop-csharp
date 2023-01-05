51b8c7901830180bcbbe57a11371084a8421382c
Alex Moroz <morozo.alexey2003@gmail.com>
2023-01-05 03:55:51 +0300

    feat: add interfaces for servise and task

---
+++ Lab5/Backups.Extra/Entities/IBackupServiceExtra.cs
@@ -0,0 +1,18 @@
+﻿using Backups.Interfaces;
+using Backups.Models;
+using Zio;
+
+namespace Backups.Extra.Entities;
+
+public interface IBackupServiceExtra
+{
+    public IBackupTaskExtra CreateTask(IConfig config);
+    public IRestorePoint RunTask(IBackupTask task);
+    public void RestoreBackup(IBackupTaskExtra task, IRestorePoint restorePoint);
+    public void RestoreBackupTo(IBackupTaskExtra task, IRestorePoint restorePoint, Repository destRepo, UPath destFolder);
+    public void AddObjectsToTask(IBackupTask task, params IBackupObject[] objects);
+    public void RemoveObjectsFromTask(IBackupTask task, params IBackupObject[] objects);
+    public void LogToConsole(bool addTimeStamp);
+    public void LogToFile(string path, bool addTimeStamp);
+    public void PurgeRestorePoints(IBackupTaskExtra task);
+}
---
+++ Lab5/Backups.Extra/Entities/IBackupTaskExtra.cs
@@ -0,0 +1,14 @@
+﻿using Backups.Interfaces;
+using Backups.Models;
+using Zio;
+
+namespace Backups.Extra.Entities;
+
+public interface IBackupTaskExtra : IBackupTask
+{
+    public ILimitAlgorithm LimitAlgorithm { get; set; }
+    public bool MergePoints { get; set; }
+    public void RestoreFromPoint(IRestorePoint rp);
+    public void RestoreFromPointTo(IRestorePoint rp, Repository destRepo, UPath destFolder);
+    public void ClearPoints();
+}
